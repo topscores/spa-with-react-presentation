033c8f1e4c72caf8d80e4e502941e2a6565c97a3
Arnupharp Viratanapanu <Arnupharp Viratanapanu>
2016-09-16 17:36:22 +0700

    Hello, todos

--- presentation/index.js
+++ presentation/index.js
@@ -6,6 +6,7 @@ import {
   Appear,
   BlockQuote,
   Cite,
+  Code,
   CodePane,
   Deck,
   Fill,
@@ -23,6 +24,8 @@ import {
   Text
 } from "spectacle";
 
+import CodeSlide from "spectacle-code-slide";
+
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
 
@@ -39,21 +42,19 @@ require("spectacle/lib/themes/default/index.css");
 
 const images = {
   reactLogo: require("../assets/react-logo.png"),
+  step1Png: require("../assets/step1/step1.png"),
   traditionArch: require("../assets/tradition.png"),
   ajaxArch: require("../assets/ajax.png"),
-  spaArch: require("../assets/spa.png"),
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  spaArch: require("../assets/spa.png")
 };
 
 preloader(images);
 
-const highlight = "#61dafb";
-const dark = "#3d3d3d";
+const primaryText = "#61dafb";
+const primaryBg = "#3d3d3d";
 const theme = createTheme({
-  primary: highlight
+  primary: primaryText,
+  secondary: primaryBg
 });
 
 export default class Presentation extends React.Component {
@@ -61,7 +62,7 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor={dark}>
+          <Slide transition={["zoom"]} bgColor="secondary">
             <Image src={images.reactLogo.replace("/", "")} margin="0px auto 20px" height="250px"/>
             <Heading size={1} fit caps lineHeight={1} textColor="primary">
               React and Redux
@@ -73,12 +74,75 @@ export default class Presentation extends React.Component {
             <Text textSize="1.2em" margin="10px 0px 0px" bold textColor="primary">Kasetsart University Bangkhen</Text>
             <Text textSize="1.2em" margin="50px 0px 0px" bold textColor="white">Arnupharp Viratanapanu</Text>
           </Slide>
+          <Slide transition={["slide"]} bgColor="secondary">
+            <Heading margin="0 auto 30px" size={1} caps textColor="primary">
+              Let's start
+            </Heading>
+            <Appear>
+              <Text textAlign="left">
+                <Code textAligh="left" textSize={28} textColor="white">
+                  git clone git@github.com:topscores/todo-react-redux.git
+                </Code>
+              </Text>
+            </Appear>
+            <Appear>
+              <Text textAlign="left">
+                <Code textAligh="left" textSize={28} textColor="white">
+                  cd todo-react-redux
+                </Code>
+              </Text>
+            </Appear>
+            <Appear>
+              <Text textAlign="left">
+                <Code textSize={28} textColor="white">
+                  npm install
+                </Code>
+              </Text>
+            </Appear>
+            <Appear>
+              <Text textAlign="left">
+                <Code textSize={28} textColor="white">
+                  npm start
+                </Code>
+              </Text>
+            </Appear>
+          </Slide>
+          <Slide transition={["zoom", "slide"]}>
+            <Heading size={1} margin="0 auto 30px" textColor="white">
+              Hello, todos!!!
+            </Heading>
+            <Image src={images.step1Png.replace("/", "")} margin="0px auto 20px" height="400px"/>
+          </Slide>
+          <CodeSlide
+            transition={["zoom"]}
+            lang="js"
+            code={require("raw!../assets/step1/.babelrc.example")}
+            margin="20px auto"
+            ranges={[
+              { loc: [0, 6], title: ".babelrc" },
+              { loc: [2, 3], title: ".babelrc" },
+              { loc: [3, 4], title: ".babelrc" }
+            ]}
+          />
+          <CodeSlide
+            transition={["zoom"]}
+            lang="js"
+            code={require("raw!../assets/step1/webpack.config.js.example")}
+            margin="20px auto"
+            ranges={[
+              { loc: [0, 17], title: "webpack.config.js" },
+              { loc: [1, 2], title: "application entry point" },
+              { loc: [2, 5], title: "bundle dependencies to bundle.js" },
+              { loc: [7, 11], title: "load .jsx using babel-loader" },
+              { loc: [11, 15], title: "load .css using css-loader and style-loader" }
+            ]}
+          />
           <Slide transition={["slide"]}>
             <Heading size={1} caps fit textColor="white" textFont="primary">
               Let's talk about history
             </Heading>
           </Slide>
-          <Slide transition={["slide"]} bgColor={dark}>
+          <Slide transition={["slide"]} bgColor="secondary">
             <Heading size={1} fit caps margin="0 auto 50px" textColor="primary">
               Traditional web application
             </Heading>
@@ -86,7 +150,7 @@ export default class Presentation extends React.Component {
               <Image src={images.traditionArch.replace("/", "")} margin="0px auto 20px" height="500px"/>
             </Layout>
           </Slide>
-          <Slide transition={["slide"]} bgColor={dark}>
+          <Slide transition={["slide"]} bgColor="secondary">
             <Heading size={1} fit caps margin="0 auto 50px" textColor="primary">
               With the help of AJAX
             </Heading>
@@ -94,7 +158,7 @@ export default class Presentation extends React.Component {
               <Image src={images.ajaxArch.replace("/", "")} margin="0px auto 20px" height="500px"/>
             </Layout>
           </Slide>
-          <Slide transition={["slide"]} bgColor={dark}>
+          <Slide transition={["slide"]} bgColor="secondary">
             <Heading size={1} fit caps margin="0 auto 50px" textColor="primary">
               Single Page Application
             </Heading>
@@ -102,93 +166,10 @@ export default class Presentation extends React.Component {
               <Image src={images.spaArch.replace("/", "")} margin="0px auto 20px" height="500px"/>
             </Layout>
           </Slide>
-          <Slide transition={["slide"]} bgColor={dark}>
+          <Slide textAlign="top" transition={["slide"]} bgColor="secondary">
             <Heading size={1} fit caps margin="0 auto 50px" textColor="primary">
               Programming before it was cool
             </Heading>
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
           </Slide>
         </Deck>
       </Spectacle>
